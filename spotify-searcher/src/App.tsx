eb34cfd17ad2c14acc5c0d3b50526fafee85b925
ioSquishy <squishylxix@gmail.com>
2026-03-03 10:20:37 -0800

    added template for form submission logic

--- spotify-searcher/src/App.tsx
+++ spotify-searcher/src/App.tsx
@@ -1,18 +1,32 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import './App.css'
 
 function App() {
+  const [songQuery, setSongQuery] = useState("");
+  const songSearchRef = useRef<HTMLInputElement>(null);
+
+  function querySong(event: React.FormEvent<HTMLFormElement>): void {
+    event.preventDefault();
+    if (songSearchRef.current && songSearchRef.current.value !== "") {
+      setSongQuery(songSearchRef.current.value);
+    }
+  }
+
+  useEffect(() => {
+    // runs whenever songQuery is updated
+    console.log(songQuery);
+  }, [songQuery])
 
   return (
-    <>
+    <div>
       <h1>Spotify Searcher</h1>
-      <form>
+      <form onSubmit={querySong}>
         <label htmlFor="track">Song name:</label>
-        <input required id="track" name="track" type="search"></input>
+        <input ref={songSearchRef} required id="track" name="track" type="search"></input>
 
         <button type="submit">Search</button>
       </form>
-    </>
+    </div>
   )
 }
 
